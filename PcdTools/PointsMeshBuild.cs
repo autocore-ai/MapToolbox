89fe0182b58018e5daaf826c292c1af69aec6795
Heliuuuu <h_l_heliu@163.com>
2020-01-14 16:33:52 +0800

    Update PointsMeshBuild.cs

--- PcdTools/PointsMeshBuild.cs
+++ PcdTools/PointsMeshBuild.cs
@@ -28,7 +28,7 @@ namespace Packages.MapToolbox.PcdTools
 {
     public static class PointsMeshBuild
     {
-        const float max_intensity = 100;
+        const float max_intensity = 50;
         public static Mesh PointsMesh(this PcdReader pcdReader)
         {
             if (pcdReader.PcdData_xyz.IsCreated)
